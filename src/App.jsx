06e654c84b4f15682e84bb71af8c3f703f045a09
Sameer Watts <sameerwatts00@gmail.com>
2023-11-22 20:18:34 +0530

    Add timer

--- src/App.jsx
+++ src/App.jsx
@@ -1,105 +1,12 @@
-import { useCallback, useEffect, useRef, useState } from "react";
-
-import Places from "./components/Places.jsx";
-import { AVAILABLE_PLACES } from "./data.js";
-import Modal from "./components/Modal.jsx";
-import DeleteConfirmation from "./components/DeleteConfirmation.jsx";
-import logoImg from "./assets/logo.png";
-import { sortPlacesByDistance } from "./loc.js";
-
-const storedIds = JSON.parse(localStorage.getItem("selectedPlaces")) || [];
-const selectedPlaces = storedIds.map((id) =>
-  AVAILABLE_PLACES.find((place) => place.id === id)
-);
+import Header from "./components/Header";
+import Quiz from "./components/Quiz";
 
 function App() {
-  const modal = useRef();
-  const [isOpen, setIsOpen] = useState(false);
-  const selectedPlace = useRef();
-  const [pickedPlaces, setPickedPlaces] = useState(selectedPlaces);
-  const [availablePlaces, setAvailablePlaces] = useState([]);
-
-  useEffect(() => {
-    navigator.geolocation.getCurrentPosition((position) => {
-      const sortedPlaces = sortPlacesByDistance(
-        AVAILABLE_PLACES,
-        position.coords.latitude,
-        position.coords.longitude
-      );
-
-      setAvailablePlaces(sortedPlaces);
-    });
-  }, []);
-
-  function handleStartRemovePlace(id) {
-    setIsOpen(true);
-    selectedPlace.current = id;
-  }
-
-  function handleStopRemovePlace() {
-    setIsOpen(false);
-  }
-
-  function handleSelectPlace(id) {
-    setPickedPlaces((prevPickedPlaces) => {
-      if (prevPickedPlaces.some((place) => place.id === id)) {
-        return prevPickedPlaces;
-      }
-      const place = AVAILABLE_PLACES.find((place) => place.id === id);
-      return [place, ...prevPickedPlaces];
-    });
-
-    const storedIds = JSON.parse(localStorage.getItem("selectedPlaces")) || [];
-    if (storedIds.indexOf(id) === -1) {
-      localStorage.setItem(
-        "selectedPlaces",
-        JSON.stringify([id, ...storedIds])
-      );
-    }
-  }
-
-  const handleRemovePlace =useCallback(function handleRemovePlace() {
-    setPickedPlaces((prevPickedPlaces) =>
-      prevPickedPlaces.filter((place) => place.id !== selectedPlace.current)
-    );
-    setIsOpen(false);
-
-    const storedIds = JSON.parse(localStorage.getItem("selectedPlaces")) || [];
-    localStorage.setItem(
-      "selectedPlaces",
-      JSON.stringify(storedIds.filter((id) => id !== selectedPlace.current))
-    );
-  }, []) 
-
   return (
     <>
-      <Modal open={isOpen} onClose={handleStopRemovePlace}>
-        <DeleteConfirmation
-          onCancel={handleStopRemovePlace}
-          onConfirm={handleRemovePlace}
-        />
-      </Modal>
-
-      <header>
-        <img src={logoImg} alt="Stylized globe" />
-        <h1>PlacePicker</h1>
-        <p>
-          Create your personal collection of places you would like to visit or
-          you have visited.
-        </p>
-      </header>
+      <Header />
       <main>
-        <Places
-          title="I'd like to visit ..."
-          fallbackText={"Select the places you would like to visit below."}
-          places={pickedPlaces}
-          onSelectPlace={handleStartRemovePlace}
-        />
-        <Places
-          title="Available Places"
-          places={availablePlaces}
-          onSelectPlace={handleSelectPlace}
-        />
+        <Quiz />
       </main>
     </>
   );
---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import quizLogo from "../assets/quiz-logo.png";
+
+const Header = () => {
+    return (
+        <header>
+            <img src={quizLogo} alt="Quiz logo" />
+            <h1>ReactQuiz</h1>
+        </header>
+    );
+};
+
+export default Header;
