86f50b43266f5f5ffd51cc74c662c801620c3559
Sameer Watts <sameerwatts00@gmail.com>
2023-11-25 02:47:32 +0530

    Partially complete food ordering app

---
+++ src/components/Meals/AvailableMeals.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { DUMMY_MEALS } from "../../assets/dummy-meals";
+import classes from "./AvailableMeals.module.css";
+import Card from "../UI/Card";
+import MealItem from "./MealItem/MealItem";
+
+const AvailableMeals = () => {
+  const mealsList = DUMMY_MEALS.map((meal) => (
+    <MealItem
+      key={meal.id}
+      id={meal.id}
+      name={meal.name}
+      description={meal.description}
+      price={meal.price}
+    />
+  ));
+  return (
+    <section className={classes.meals}>
+      <Card>
+        <ul>{mealsList}</ul>
+      </Card>
+    </section>
+  );
+};
+
+export default AvailableMeals;
