8227974b79fa0c45ec06326895cffa4964f2f54d
Sameer Watts <sameerwatts00@gmail.com>
2023-12-11 23:44:40 +0530

    Fetch selected places and display on screen

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,37 @@
-import { useRef, useState, useCallback } from "react";
+import { useRef, useState, useCallback, useEffect } from "react";
 
 import Places from "./components/Places.jsx";
 import Modal from "./components/Modal.jsx";
 import DeleteConfirmation from "./components/DeleteConfirmation.jsx";
 import logoImg from "./assets/logo.png";
 import AvailablePlaces from "./components/AvailablePlaces.jsx";
-import { updateUserPlaces } from "./http.js";
+import { fetchUserPlaces, updateUserPlaces } from "./http.js";
 import Error from "./components/Error.jsx";
 
 function App() {
   const selectedPlace = useRef();
 
   const [userPlaces, setUserPlaces] = useState([]);
+  const [isFeching, setIsFetching] = useState(false);
+  const [error, setError] = useState("");
+
   const [errorUpdatingPlaces, setErrorUpdatingPlaces] = useState();
 
   const [modalIsOpen, setModalIsOpen] = useState(false);
+
+  useEffect(() => {
+    async function fetchPlaces() {
+      setIsFetching(true);
+      try {
+        const places = await fetchUserPlaces();
+        setUserPlaces(places);
+      } catch (err) {
+        setError({ message: err.message || "Failed to fetch user places" });
+      }
+      setIsFetching(false);
+    }
+    fetchPlaces();
+  }, []);
 
   function handleStartRemovePlace(place) {
     setModalIsOpen(true);
@@ -100,12 +117,23 @@ function App() {
         </p>
       </header>
       <main>
-        <Places
-          title="I'd like to visit ..."
-          fallbackText="Select the places you would like to visit below."
-          places={userPlaces}
-          onSelectPlace={handleStartRemovePlace}
-        />
+        {error && (
+          <Error
+            title="An error occured"
+            message={error.message}
+            onConfirm={() => {}}
+          />
+        )}
+        {!error && (
+          <Plafces
+            title="I'd like to visit ..."
+            fallbackText="Select the places you would like to visit below."
+            places={userPlaces}
+            onSelectPlace={handleStartRemovePlace}
+            isLoading={isFeching}
+            loadingText="Fetching your places..."
+          />
+        )}
 
         <AvailablePlaces onSelectPlace={handleSelectPlace} />
       </main>
--- src/http.js
+++ src/http.js
@@ -8,11 +8,11 @@ export async function fetchAvailablePlaces() {
 
   return resData.places;
 }
-
 export async function updateUserPlaces(places) {
   const response = await fetch("http://localhost:3000/user-places", {
     method: "PUT",
     body: JSON.stringify({ places }),
+
     headers: { "Content-Type": "application/json" },
   });
   const resData = response.json();
@@ -21,3 +21,15 @@ export async function updateUserPlaces(places) {
   }
   return resData.message;
 }
+
+export async function fetchUserPlaces() {
+    const response = await fetch("http://localhost:3000/user-places");
+    const resData = await response.json();
+  
+    if (!response.ok) {
+      throw new Error("Failed to fetch user places");
+    }
+  
+    return resData.places;
+  }
+  
