46d9a21f36d49609ca207fdfd51f65c0eccdb513
Sameer Watts <sameerwatts00@gmail.com>
2023-12-11 18:30:31 +0530

    Fetching data with loading

--- src/components/AvailablePlaces.jsx
+++ src/components/AvailablePlaces.jsx
@@ -1,12 +1,29 @@
-import Places from './Places.jsx';
+import { useEffect, useState } from "react";
+import Places from "./Places.jsx";
 
 export default function AvailablePlaces({ onSelectPlace }) {
+  const [availablePlaces, setAvailablePlaces] = useState([]);
+  const [isFeching, setIsFetching] = useState(false);
+
+  useEffect(() => {
+    const fetchPlaces = async () => {
+      setIsFetching(true);
+      const response = await fetch("http://localhost:3000/places");
+      const resData = await response.json();
+      setAvailablePlaces(resData.places);
+      setIsFetching(false);
+    };
+    fetchPlaces();
+  }, []);
+
   return (
     <Places
       title="Available Places"
-      places={[]}
+      places={availablePlaces}
       fallbackText="No places available."
       onSelectPlace={onSelectPlace}
+      loadingText="Fetching place data..."
+      isLoading={isFeching}
     />
   );
 }
