195836ad6cc09e3e175a357059a7b6be2b76d78d
Sameer Watts <sameerwatts00@gmail.com>
2023-12-12 18:48:04 +0530

    Fetch data in useeffect

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 
 import MoviesList from "./components/MoviesList";
 import "./App.css";
@@ -7,7 +7,7 @@ function App() {
   const [movies, setMovies] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
-  async function fetchMoviesHandler() {
+  const fetchMoviesHandler = useCallback(async () => {
     setIsLoading(true);
     setMovies([]);
     setError(null);
@@ -18,7 +18,6 @@ function App() {
         throw new Error("Failed to fetch movies");
       }
       const resData = await response.json();
-
 
       const transformedMovies = resData.results.map((movieData) => ({
         id: movieData.episode_id,
@@ -31,21 +30,25 @@ function App() {
       setError(err.message || "Failed to fetch movies");
     }
     setIsLoading(false);
+  }, []);
+
+  let content = <p>Click button to fetch movies</p>;
+
+  if (isLoading) {
+    content = <h2 style={{ textAlign: "center" }}>Loading movies...</h2>;
   }
 
-  let content = <p>Click button to fetch movies</p>
-
-  if(isLoading) {
-    content = <h2 style={{ textAlign: "center" }}>Loading movies...</h2>
-  }
-
-  if(movies.length > 0) {
+  if (movies.length > 0) {
     content = <MoviesList movies={movies} />;
   }
 
-  if(error) {
-    content =  <p>{error}</p>;
+  if (error) {
+    content = <p>{error}</p>;
   }
+
+  useEffect(() => {
+    fetchMoviesHandler();
+  }, [fetchMoviesHandler]);
   return (
     <React.Fragment>
       <section>
@@ -53,19 +56,7 @@ function App() {
           Fetch Movies
         </button>
       </section>
-      {
-        <section>
-          {/* {!isLoading && movies.length === 0 && !error && (
-            <p>Click button to fetch movies</p>
-          )}
-          {isLoading && movies.length === 0 && (
-            <h2 style={{ textAlign: "center" }}>Loading movies...</h2>
-          )}
-          {!isLoading && movies.length > 0 && <MoviesList movies={movies} />}
-          {error && <p>{error}</p>} */}
-          {content}
-        </section>
-      }
+      {<section>{content}</section>}
     </React.Fragment>
   );
 }
