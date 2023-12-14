44780d61f46919ed49433531b93926eaf6615250
Sameer Watts <sameerwatts00@gmail.com>
2023-12-14 12:34:08 +0530

    Complete custom hook part 2

--- src/components/BackwardCounter.js
+++ src/components/BackwardCounter.js
@@ -1,17 +1,21 @@
-import { useState, useEffect } from 'react';
-
-import Card from './Card';
+import Card from "./Card";
+import useCounter from "../hooks/useCounter";
 
 const BackwardCounter = () => {
-  const [counter, setCounter] = useState(0);
+  // const [counter, setCounter] = useState(0);
 
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setCounter((prevCounter) => prevCounter - 1);
-    }, 1000);
+  // useEffect(() => {
+  //   const interval = setInterval(() => {
+  //     setCounter((prevCounter) => prevCounter - 1);
+  //   }, 1000);
 
-    return () => clearInterval(interval);
-  }, []);
+  //   return () => clearInterval(interval);
+  // }, []);
+
+  function decrement(prevValue) {
+    return prevValue - 1;
+  }
+  const counter = useCounter(decrement);
 
   return <Card>{counter}</Card>;
 };
