7a32926d3858d56497d48d5afbbcf402ebfda5a1
Sameer Watts <sameerwatts00@gmail.com>
2023-12-15 16:22:28 +0530

    Add stateLogin file

---
+++ src/components/StateLogin.jsx
@@ -0,0 +1,56 @@
+import { useState } from "react";
+
+export default function Login() {
+  const [enteredValues, setEnteredValues] = useState({
+    email: "",
+    password: "",
+  });
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log("Submitted");
+    const { email, password } = enteredValues;
+    console.log("user email and password", email, password);
+  };
+
+  const handleValueChange = (e, indetifier) => {
+    setEnteredValues((prev) => {
+      return { ...prev, [indetifier]: e.target.value };
+    });
+  };
+ 
+  return (
+    <form onSubmit={handleSubmit}>
+      <h2>Login</h2>
+
+      <div className="control-row">
+        <div className="control no-margin">
+          <label htmlFor="email">Email</label>
+          <input
+            id="email"
+            type="email"
+            name="email"
+            onChange={(e) => handleValueChange(e, "email")}
+            value={enteredValues.email}
+          />
+        </div>
+
+        <div className="control no-margin">
+          <label htmlFor="password">Password</label>
+          <input
+            id="password"
+            type="password"
+            name="password"
+            onChange={(e) => handleValueChange(e, "password")}
+            value={enteredValues.password}
+          />
+        </div>
+      </div>
+
+      <p className="form-actions">
+        <button className="button button-flat">Reset</button>
+        <button className="button">Login</button>
+      </p>
+    </form>
+  );
+}
