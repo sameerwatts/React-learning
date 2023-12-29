5f5cb2a0de1f37ed030629c613f8cba9a5b19213
Sameer Watts <sameerwatts00@gmail.com>
2023-12-29 12:20:29 +0530

    Add custom http hook

--- src/components/Meals.jsx
+++ src/components/Meals.jsx
@@ -1,25 +1,15 @@
-import React, { useEffect, useState } from "react";
+import useHttp from "../hooks/useHttp";
 import MealItem from "./MealItem";
 
 const Meals = () => {
-  const [loadedMeals, setLoadedMeals] = useState([]);
-  useEffect(() => {
-    async function fetchMeals() {
-      const response = await fetch("http://localhost:3000/meals");
+  const {data: loadedMeals, isLoading, error} = useHttp("http://localhost:3000/meals")
 
-      if (!response.ok) {
-        //...
-      }
-
-      const meals = await response.json();
-      setLoadedMeals(meals);
-    }
-    fetchMeals();
-  }, []);
-
+  if(isLoading) {
+    return <p>Loading Meals...</p>
+  }
   return (
     <ul id="meals">
-      {loadedMeals.map((meal) => (
+      {loadedMeals?.map((meal) => (
         <MealItem key={meal.id} meal={meal} />
       ))}
     </ul>
---
+++ src/hooks/useHttp.js
@@ -0,0 +1,42 @@
+import { useCallback, useEffect, useState } from "react";
+
+async function sendHttpRequest(url, config) {
+  const response = await fetch(url, config);
+
+  const resData = await response.json();
+
+  if (!response.ok) {
+    throw new Error(resData.message || "Unable to load the request");
+  }
+
+  return resData;
+}
+const useHttp = (url, config) => {
+  const [data, setData] = useState();
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState();
+  const sendRequest = useCallback(async function sendRequest() {
+    setIsLoading(true);
+    try {
+      const resData = await sendHttpRequest(url, config);
+      setData(resData);
+    } catch (error) {
+      setError(error.message || "Something went wrong");
+    }
+    setIsLoading(false);
+  },[url, config])
+
+  useEffect(() => {
+    if(!config || config.method === 'GET') {
+        sendRequest();
+    }
+  },[sendRequest])
+  return {
+    data,
+    isLoading,
+    error,
+    sendRequest, 
+  }
+};
+
+export default useHttp;
