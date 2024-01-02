c09eddf254a9000b083b1b6b54ef8a4774202c04
Sameer Watts <sameerwatts00@gmail.com>
2024-01-02 18:30:08 +0530

    splitting code

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { useDispatch, useSelector } from "react-redux";
 import classes from "./Counter.module.css";
-import { counterActions } from "../store";
+import { counterActions } from "../store/counter";
 
 const Counter = () => {
   const counter = useSelector((state) => state.counter.counter);
