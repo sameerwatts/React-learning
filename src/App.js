1784fa6d499c0fcdb24dc12bf2ded8961251128b
Sameer Watts <sameerwatts00@gmail.com>
2024-01-02 16:13:30 +0530

    Dispatching from actions inside component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
-import Counter from './components/Counter';
-
+import React from "react";
+import Counter from "./components/Counter";
 
 function App() {
-  return (
-    <Counter />
-  );
+  return <Counter />;
 }
 
 export default App;
