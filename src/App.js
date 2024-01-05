10b81b56b4b7d673e0bc0810d0b66ec0be13d2fa
Sameer Watts <sameerwatts00@gmail.com>
2024-01-05 15:00:58 +0530

    Layouts and nested routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Home from "./pages/Home";
 import Products from "./pages/Products";
+import Root from "./pages/Root";
 
 const router = createBrowserRouter([
-  { path: "/", element: <Home /> },
-  { path: "/products", element: <Products /> },
+  {
+    path: "/",
+    element: <Root />,
+    children: [
+      { path: "/", element: <Home /> },
+      { path: "/products", element: <Products /> },
+    ],
+  },
 ]);
 
 function App() {
