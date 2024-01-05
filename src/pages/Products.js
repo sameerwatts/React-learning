6f58bf01b7015f703d91ad626a87555604b468b3
Sameer Watts <sameerwatts00@gmail.com>
2024-01-05 14:35:53 +0530

    Navigating bwtween pages with links

--- src/pages/Products.js
+++ src/pages/Products.js
@@ -1,7 +1,11 @@
 import React from "react";
 
 const Products = () => {
-  return <h1>The Product Page</h1>;
+  return (
+    <>
+      <h1>The Product Page</h1>
+    </>
+  );
 };
 
 export default Products;
