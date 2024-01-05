b13b42a1749424e97aac60e0825a21fe8f6595bb
Sameer Watts <sameerwatts00@gmail.com>
2024-01-05 17:52:30 +0530

    Work wth index routes

--- src/App.js
+++ src/App.js
@@ -11,9 +11,9 @@ const router = createBrowserRouter([
     element: <Root />,
     errorElement: <Error />,
     children: [
-      { path: "", element: <Home /> },
+      { index: true, element: <Home /> },
       { path: "products", element: <Products /> },
-      { path: "products/:productId", element: <ProductDetail /> }
+      { path: "products/:productId", element: <ProductDetail /> },
     ],
   },
 ]);
