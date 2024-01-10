883598f63c37305a17187c0f1b2d70ff75db852b
Sameer Watts <sameerwatts00@gmail.com>
2024-01-10 12:34:10 +0530

    More loader data usage

--- frontend/src/pages/Events.js
+++ frontend/src/pages/Events.js
@@ -3,6 +3,8 @@ import EventsList from "../components/EventsList";
 
 function EventsPage() {
   const events = useLoaderData();
+  // useLoaderData can be used in the same level or at the lower(Child) level, we can't use useLoaderData at the higher level
+
   return <EventsList events={events} />;
 }
 
